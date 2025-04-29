9135656c192a2e315cb47e36797824f2c00a5fbc
rayfong61 <rayfong61@gmai.com>
2025-04-29 16:55:42 +0800

    建立顧客登入/註冊/預約API

---
+++ backend/queries.sql
@@ -0,0 +1,21 @@
+CREATE TABLE orders (
+    id SERIAL PRIMARY KEY,
+    client_id INTEGER REFERENCES client(id),  -- 顧客id
+    booking_detail JSONB,                  -- 預約的詳細內容
+    total_price INTEGER,                   -- 總金額
+    total_duration INTEGER,                -- 總時長
+    booking_date DATE,                     -- 預約日期
+    booking_time TIME,                     -- 預約時間
+    created_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE client (
+	id SERIAL PRIMARY KEY,                      -- 顧客id
+	client_name VARCHAR(100) NOT NULL,          -- 顧客姓名
+	contact_mobile VARCHAR(20),                 -- 手機
+	contact_mail VARCHAR(100) UNIQUE,           -- e-mail
+	birthday DATE,                              -- 生日
+	address VARCHAR(255),                       -- 地址
+	password VARCHAR(255) NOT NULL,             -- 密碼
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
