c3fe878830315f4dc586a5a7969cbf3865249b6e
rayfong61 <rayfong61@gmail.com>
2025-05-17 16:23:41 +0800

    test-vercel3

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ dotenv.config();
 const app = express();
 const saltRounds = 10;
 const PORT = process.env.PORT || 5000;
-const FRONTEND_URL = process.env.FRONTEND_URL;
+const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:3000";
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
@@ -189,23 +189,6 @@ function ensureAuthenticated(req, res, next) {
   res.status(401).json({ message: "請先登入" });
 }
 
-// 預約歷史查詢 API
-// app.get("/orders", ensureAuthenticated, async (req, res) => {
-//   const { client_id } = req.query;
-
-//   try {
-//     const result = await pool.query(
-//       `SELECT * FROM orders WHERE client_id = $1 ORDER BY booking_date DESC, booking_time DESC`,
-//       [client_id]
-//     );
-
-//     res.json(result.rows);
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ message: "查詢預約失敗" });
-//   }
-// });
-
 
 // 測試:檢查 session 是否維持
 app.get("/me", (req, res) => {
@@ -223,12 +206,6 @@ app.get("/", (req, res) => {
 
 
 
-
-
-
-
-
-
 app.get("/auth/line", (req, res, next) => {
   const redirect = req.query.redirect || "/account";
   req.session.redirectAfterLogin = redirect;
@@ -242,10 +219,10 @@ app.get("/auth/line/callback", passport.authenticate("line", {
   const html = `
     <script>
       if (window.opener) {
-        window.opener.postMessage("login-success", "http://localhost:3000");
+        window.opener.postMessage("login-success", "${FRONTEND_URL}");
         window.close();
       } else {
-        window.location.href = "http://localhost:3000/account";
+        window.location.href = "${FRONTEND_URL}/account";
       }
     </script>
   `;
@@ -268,10 +245,10 @@ app.get("/auth/google/callback", passport.authenticate("google", {
   const html = `
     <script>
       if (window.opener) {
-        window.opener.postMessage("login-success", "http://localhost:3000");
+        window.opener.postMessage("login-success", "${FRONTEND_URL}");
         window.close();
       } else {
-        window.location.href = "http://localhost:3000/account";
+        window.location.href = "${FRONTEND_URL}/account";
       }
     </script>
   `;
